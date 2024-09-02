643300ac414449f0e3ed9d9f6769aa4cfaf54163
JJampion <nightodaygamestudio@gmail.com>
2024-09-02 18:07:25 +0200

    PlayerNetwork

---
+++ Assets/02_Scripts/02_P2P_SC/PlayerNetwork.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+    private void Update() {
+            Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+}
