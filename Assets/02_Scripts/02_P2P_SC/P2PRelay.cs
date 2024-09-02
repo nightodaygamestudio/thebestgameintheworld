f7b72d41c3ba322d1e56f22b8e7eb50ae06f140e
JJampion <nightodaygamestudio@gmail.com>
2024-09-02 15:54:38 +0200

    test

--- Assets/02_Scripts/02_P2P_SC/P2PRelay.cs
+++ Assets/02_Scripts/02_P2P_SC/P2PRelay.cs
@@ -5,7 +5,7 @@ public class P2PRelay : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
