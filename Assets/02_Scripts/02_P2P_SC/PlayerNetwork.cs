be407a6b219cf91222218aa97649a6d8a1f68895
JJampion <nightodaygamestudio@gmail.com>
2024-09-02 20:16:29 +0200

    PlayerNetwork

--- Assets/02_Scripts/02_P2P_SC/PlayerNetwork.cs
+++ Assets/02_Scripts/02_P2P_SC/PlayerNetwork.cs
@@ -1,11 +1,51 @@
 using Unity.Netcode;
+using Unity.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.PackageManager;
 
 
 public class PlayerNetwork : NetworkBehaviour {
 
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn() { 
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
     private void Update() {
+
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            PrototypeClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
+            /*randomNumber.Value = new MyCustomData {
+                _int = 10,
+                _bool = false,
+                message = "Nice Cock Miko"
+            };*/
+        }
+
+
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -18,4 +58,13 @@ public class PlayerNetwork : NetworkBehaviour {
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 
+    [ServerRpc]
+    private void PrototypeServerRpc(ServerRpcParams serverRpcParams) {
+        Debug.Log("PrototypeServerRpc " + OwnerClientId + ", " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void PrototypeClientRpc(ClientRpcParams prototypeRpcParams) {
+        Debug.Log("PrototypeClientRpc");
+    }
 }
