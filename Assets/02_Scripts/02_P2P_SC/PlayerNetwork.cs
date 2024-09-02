8f532a6ec26a6092c8b55bdf23dc80fcad88ef30
JJampion <nightodaygamestudio@gmail.com>
2024-09-02 18:27:08 +0200

    Multiplayer Samples Utilities

--- Assets/02_Scripts/02_P2P_SC/PlayerNetwork.cs
+++ Assets/02_Scripts/02_P2P_SC/PlayerNetwork.cs
@@ -1,9 +1,13 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour {
+
+public class PlayerNetwork : NetworkBehaviour {
 
     private void Update() {
-            Vector3 moveDir = new Vector3(0, 0, 0);
+        if (!IsOwner) return;
+
+        Vector3 moveDir = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
